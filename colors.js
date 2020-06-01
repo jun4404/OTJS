a807cd4f009f46449b2d23ae76d0582fe11c1756
JUN <jun4404@gmail.com>
2020-06-01 17:46:23 +0900

    script -> JS file

---
+++ colors.js
@@ -0,0 +1,34 @@
+var Links = {
+  setColor: function (color) {
+    var alist = document.querySelectorAll("a");
+    var i = 0;
+    while (i < alist.length) {
+      console.log(alist[i]);
+      alist[i].style.color = color;
+      i = i + 1;
+    }
+  },
+};
+var Body = {
+  setColor: function (color) {
+    document.querySelector("body").style.color = color;
+  },
+  setBackgroundColor: function (color) {
+    document.querySelector("body").style.backgroundColor = color;
+  },
+};
+
+function nightDayHandler(self) {
+  var target = document.querySelector("body");
+  if (self.value === "night") {
+    Body.setColor("white");
+    Body.setBackgroundColor("black");
+    self.value = "day";
+    Links.setColor("powderblue");
+  } else {
+    Body.setColor("black");
+    Body.setBackgroundColor("white");
+    self.value = "night";
+    Links.setColor("blue");
+  }
+}
