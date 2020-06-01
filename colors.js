1ae2ee178be3daebfdfed5a8ea114d3b31dafaf5
JUN <jun4404@gmail.com>
2020-06-01 18:13:14 +0900

    Jquery

--- colors.js
+++ colors.js
@@ -1,20 +1,27 @@
 var Links = {
   setColor: function (color) {
-    var alist = document.querySelectorAll("a");
-    var i = 0;
-    while (i < alist.length) {
-      console.log(alist[i]);
-      alist[i].style.color = color;
-      i = i + 1;
-    }
+    // var alist = document.querySelectorAll("a");
+    // var i = 0;
+    // while (i < alist.length) {
+    //   console.log(alist[i]);
+    //   alist[i].style.color = color;
+    //   i = i + 1;
+    // }
+
+    //jquery
+    $("a").css("color", color);
   },
 };
 var Body = {
   setColor: function (color) {
-    document.querySelector("body").style.color = color;
+    //document.querySelector("body").style.color = color;
+    //jquery
+    $("body").css("color", color);
   },
   setBackgroundColor: function (color) {
-    document.querySelector("body").style.backgroundColor = color;
+    //document.querySelector("body").style.backgroundColor = color;
+    //jquery
+    $("body").css("backgroundColor", color);
   },
 };
 
